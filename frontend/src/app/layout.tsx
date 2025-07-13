258ca48f7b4438f7c7f74d38a9c3ee120d5e4807
098ff <chanatdakc@gmail.com>
2025-07-13 22:07:01 +0700

    Init by import used lib

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
